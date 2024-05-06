eab083d10ff50ffb5a7e59fb498a96ebc7ed64c8
SeharZaheer <143021697+SeharZaheer@users.noreply.github.com>
2024-05-06 07:13:52 -0400

    Update db.py

--- app/db.py
+++ app/db.py
@@ -8,7 +8,7 @@ from gspread_models.base import BaseModel
 load_dotenv()
 
 # google credentials:
-DEFAULT_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "google-credentials.json")
+DEFAULT_FILEPATH = os.path.join(os.path.dirname(__file__), "google-credentials.json")
 GOOGLE_CREDENTIALS_FILEPATH = os.getenv("GOOGLE_CREDENTIALS_FILEPATH", default=DEFAULT_FILEPATH)
 
 # google sheets document:
